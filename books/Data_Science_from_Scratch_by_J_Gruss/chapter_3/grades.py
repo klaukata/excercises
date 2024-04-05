957688cfa9a837a8817d7f9b1e34271128f55645
kboro <kkborowy@gmail.com>
2024-04-05 17:52:38 +0200

    histogram works

--- books/Data_Science_from_Scratch_by_J_Gruss/chapter_3/grades.py
+++ books/Data_Science_from_Scratch_by_J_Gruss/chapter_3/grades.py
@@ -3,16 +3,11 @@ from collections import Counter
 
 grades=[83, 23, 95, 92, 70, 0, 77, 94, 55, 83]
 
-histogram=Counter(grade for grade in grades)
 histogram1=Counter(min(grade // 10 * 10, 90) for grade in grades)
 
-plt.scatter(grades, grades)
-# plt.bar([x + 5 for x in histogram.keys()],
-#         histogram.values(),
-#         10)
-plt.axis((-5, 120, 0, 5))
-# plt.show()
-print(histogram)
-print(histogram1)
+plt.bar([x + 5 for x in histogram1.keys()],
+         histogram1.values(),
+         10)
+plt.axis((-5, 105, 0, 5))
+
 plt.show()
-# {0: 83, 1: 23, 2: 95, ...}
