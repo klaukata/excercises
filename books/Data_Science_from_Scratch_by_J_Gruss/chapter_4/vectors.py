3a7101838ef39e86f34789bfc2b248b5a77e685e
kboro <kkborowy@gmail.com>
2024-04-06 16:45:34 +0200

    function: adding scalar vectors

--- books/Data_Science_from_Scratch_by_J_Gruss/chapter_4/vectors.py
+++ books/Data_Science_from_Scratch_by_J_Gruss/chapter_4/vectors.py
@@ -2,9 +2,13 @@ from typing import List
 
 Vector = List[float]
 
-#adding two vectors
+# adding two vectors
 def add(x: Vector, y: Vector):
     assert len(x) == len(y), 'both vectors need to have the same length'
     return list(i + j for i, j in zip(x, y))
+# print(add([1,2,3], [4,5,6]))
 
-print(add([1,2,3], [4,5,6]))
+# scalar vecor
+def scalar(c: float, v: Vector) -> Vector:
+    return list(c * v_i for v_i in v)
+print(scalar(4, [1,2,4]))
