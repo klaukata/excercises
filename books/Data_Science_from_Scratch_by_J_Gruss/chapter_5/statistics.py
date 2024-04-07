2e288166d34c25da6ddca11254f0eddafb3c92ec
kboro <kkborowy@gmail.com>
2024-04-07 16:09:17 +0200

    function: median val

--- books/Data_Science_from_Scratch_by_J_Gruss/chapter_5/statistics.py
+++ books/Data_Science_from_Scratch_by_J_Gruss/chapter_5/statistics.py
@@ -2,12 +2,27 @@ from matplotlib import pyplot as plt
 from collections import Counter
 from typing import List
 
-num_friends = [100, 32, 60, 400, 400, 32, 63, 30, 24, 21]
+num_friends = [100, 49, 41, 40, 25]
+
 
 # calculating mean
 def mean(x: List[float]) -> float:
     return sum(x) / len(x)
 
 
+# median (odd)
+def median_odd(x: List[float]) -> float:
+    l = len(x)
+    return sorted(x)[l // 2]
 
-print(mean(num_friends))
+# median (even)
+def median_even(x: List[float]) -> float:
+    srt = sorted(x)
+    l = len(x) // 2
+    return (srt[l] + srt[l+1]) / 2
+
+def median(x: List[float]) -> float:
+    return median_even(x) if len(x) % 2 == 0 else median_odd(x)
+
+
+print(median([1, 10, 2, 9, 5]))
