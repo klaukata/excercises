a6d242ef254905b6bc6b212145ba65be5fd6a9fe
kboro <kkborowy@gmail.com>
2024-04-16 16:13:55 +0200

    1.

---
+++ learning_area/sql/SQL50_leetcode.sql
@@ -0,0 +1,3 @@
+-- 1. Recyclable and Low Fat Products - https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+select product_id from products
+    where low_fats = "Y" and recyclable = "Y";
