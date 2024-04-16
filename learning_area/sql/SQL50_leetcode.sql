490150740930828074bcd9388251d92263095292
kboro <kkborowy@gmail.com>
2024-04-16 16:24:47 +0200

    2. Find Customer Referee

--- learning_area/sql/SQL50_leetcode.sql
+++ learning_area/sql/SQL50_leetcode.sql
@@ -1,3 +1,12 @@
+-- @@@ == do it again
+
 -- 1. Recyclable and Low Fat Products - https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
 select product_id from products
     where low_fats = "Y" and recyclable = "Y";
+
+-- 2. Find Customer Referee - https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+-- @@@
+select name 
+    from Customer
+    where not referee_id = 2 or referee_id is null
+    order by name desc;
