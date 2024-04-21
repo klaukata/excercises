cddb272ae21ba9a5f382148594f3085597068c91
kboro <kkborowy@gmail.com>
2024-04-21 21:55:10 +0200

    7. Students and Examinations

--- learning_area/sql/SQL50_leetcode.sql
+++ learning_area/sql/SQL50_leetcode.sql
@@ -62,3 +62,12 @@ select name, bonus
     from Employee
     left join Bonus on Employee.empId = Bonus.empId
     where bonus < 1000 or bonus is null;
+
+-- 7. Students and Examinations - https://leetcode.com/problems/students-and-examinations/description/?envType=study-plan-v2&envId=top-sql-50
+-- @@@
+select st.student_id, st.student_name, su.subject_name, count(e.subject_name) as attended_exams
+    from Students st
+    join Subjects su
+    left join Examinations e on st.student_id = e.student_id and su.subject_name = e.subject_name
+    group by st.student_id, su.subject_name
+    order by st.student_id
