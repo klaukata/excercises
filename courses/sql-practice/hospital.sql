71115b7b58f2f1f105430bb0dfdefc782eae9d2c
kboro <kkborowy@gmail.com>
2024-06-22 19:46:01 +0200

    Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.

--- courses/sql-practice/hospital.sql
+++ courses/sql-practice/hospital.sql
@@ -61,3 +61,9 @@ select
 	sum(gender = 'M') as male_count,
     sum(gender = 'F') as female_count
 from patients
+
+-- Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
+select first_name, last_name, allergies
+from patients
+where allergies in ('Penicillin', 'Morphine')
+order by allergies, first_name, last_name
