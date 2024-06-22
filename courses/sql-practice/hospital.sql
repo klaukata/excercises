f163b658501c591209e22223984415f9210f41a8
kboro <kkborowy@gmail.com>
2024-06-23 01:14:17 +0200

    We want to display each patient's full name in a single column. Their last_name in all upper letters must appear first, then first_name in all lower case letters. Separate the last_name and first_name with a comma. Order the list by the first_name in decending order

--- courses/sql-practice/hospital.sql
+++ courses/sql-practice/hospital.sql
@@ -117,3 +117,8 @@ select
 from patients
 where year(birth_date) between 1970 and 1979
 order by birth_date
+
+-- We want to display each patient's full name in a single column. Their last_name in all upper letters must appear first, then first_name in all lower case letters. Separate the last_name and first_name with a comma. Order the list by the first_name in decending order
+select concat(upper(last_name), ',',lower(first_name)) as full_name
+from patients
+order by first_name desc
