bbb11c62638b8f90d368d020db1cb18afe8013a2
kboro <kkborowy@gmail.com>
2024-06-23 01:17:33 +0200

    Show the province_id(s), sum of height; where the total sum of its patient's height is greater than or equal to 7,000.

--- courses/sql-practice/hospital.sql
+++ courses/sql-practice/hospital.sql
@@ -122,3 +122,11 @@ order by birth_date
 select concat(upper(last_name), ',',lower(first_name)) as full_name
 from patients
 order by first_name desc
+
+-- Show the province_id(s), sum of height; where the total sum of its patient's height is greater than or equal to 7,000.
+select 
+	province_id,
+    sum(height) AS sum_height
+from patients
+group by province_id
+having sum(height) >= 7000
