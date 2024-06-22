bc574666830e06501e8eb25c88bbdf0b862eca34
kboro <kkborowy@gmail.com>
2024-06-22 19:16:56 +0200

    Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.

--- courses/sql-practice/hospital.sql
+++ courses/sql-practice/hospital.sql
@@ -2,3 +2,8 @@
 SELECT * 
 FROM admissions
 where admission_date = discharge_date
+
+-- Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+SELECT patient_id, first_name 
+FROM patients
+where len(first_name) >= 6 and first_name like 'S%s'
