c211cbb0cb467a2a147ab00835b46b471412983a
kboro <kkborowy@gmail.com>
2024-06-24 18:55:35 +0200

    Show patient_id, first_name, last_name, and attending doctor's specialty. Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'

--- courses/sql-practice/hospital.sql
+++ courses/sql-practice/hospital.sql
@@ -261,3 +261,13 @@ select
     end as isObese
 from patients
 
+-- Show patient_id, first_name, last_name, and attending doctor's specialty. Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+select 
+	p.patient_id,
+    p.first_name,
+    p.last_name,
+    d.specialty
+from admissions a
+join patients p on a.patient_id = p.patient_id
+join doctors d on attending_doctor_id = d.doctor_id
+where diagnosis = 'Epilepsy' and d.first_name = 'Lisa'
