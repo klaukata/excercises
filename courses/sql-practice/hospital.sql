a77748d48a35d3d9aae56a6bbf7e489c25587d12
kboro <kkborowy@gmail.com>
2024-06-25 13:37:10 +0200

    We need a breakdown for the total amount of admissions each doctor has started each year. Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.

--- courses/sql-practice/hospital.sql
+++ courses/sql-practice/hospital.sql
@@ -358,3 +358,16 @@ order by
         else 1
     end,
     province_name
+
+-- We need a breakdown for the total amount of admissions each doctor has started each year. Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.
+select 
+	d.doctor_id,
+    concat(d.first_name, ' ', d.last_name) as doctor_name,
+    d.specialty,
+    year(a.admission_date) as selected_year,
+    count(year(a.admission_date)) as admissions_number
+from admissions a
+join doctors d on a.attending_doctor_id = d.doctor_id
+group by 
+	doctor_id,
+    year(a.admission_date)
