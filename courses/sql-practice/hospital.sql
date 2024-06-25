a736791a7a2cc9288fdbb12079391db722e9a708
kboro <kkborowy@gmail.com>
2024-06-25 13:23:57 +0200

    Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.

--- courses/sql-practice/hospital.sql
+++ courses/sql-practice/hospital.sql
@@ -347,3 +347,14 @@ select
 		as admission_amout_change
 from admissions
 group by admission_date
+
+-- Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.
+-- TODO - do again
+select province_name
+from province_names
+order by
+	case
+    	when province_name = 'Ontario' then 0
+        else 1
+    end,
+    province_name
