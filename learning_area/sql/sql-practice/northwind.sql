ba337a38ae959b787e8abccb31b914c4a1a5385d
kboro <kkborowy@gmail.com>
2024-07-17 15:30:43 +0200

    Show the employee_id, order_id, customer_id, required_date, shipped_date from all orders shipped later than the required date

--- learning_area/sql/sql-practice/northwind.sql
+++ learning_area/sql/sql-practice/northwind.sql
@@ -21,3 +21,13 @@ select
     freight
 from orders
 where order_date = '2018-02-26'
+
+-- Show the employee_id, order_id, customer_id, required_date, shipped_date from all orders shipped later than the required date
+select
+	employee_id,
+    order_id,
+    customer_id,
+    required_date,
+    shipped_date
+from orders
+where shipped_date > required_date
