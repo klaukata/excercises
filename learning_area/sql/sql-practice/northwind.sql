866be883b52470dd39dbf983d36630897017f03f
kboro <kkborowy@gmail.com>
2024-07-17 16:10:23 +0200

    Show the company_name, contact_name, fax number of all customers that has a fax number. (not null)

--- learning_area/sql/sql-practice/northwind.sql
+++ learning_area/sql/sql-practice/northwind.sql
@@ -46,3 +46,13 @@ select
 from customers
 where city like '%l%'
 order by contact_name
+
+-- Show the company_name, contact_name, fax number of all customers that has a fax number. (not null)
+select 
+	company_name,
+    contact_name,
+    fax
+from customers
+where fax not null
+
+-- 
