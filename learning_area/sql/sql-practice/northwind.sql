40731e72678ef2235d076795df61d1496b11e8f6
kboro <kkborowy@gmail.com>
2024-07-18 10:50:20 +0200

    Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken

--- learning_area/sql/sql-practice/northwind.sql
+++ learning_area/sql/sql-practice/northwind.sql
@@ -83,3 +83,26 @@ from customers
 union
 select city, company_name, contact_name, 'suppliers' as relationship
 from suppliers
+
+-- Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken, 
+-- and a column called "Shipped" that displays "On Time" if the order shipped_date is less or equal to the 
+-- required_date, "Late" if the order shipped late.
+-- Order by employee last_name, then by first_name, and then descending by number of orders.
+-- TODO
+select 
+	first_name,
+    last_name,
+	count(e.employee_id) as num_orders,
+    case 
+    	when shipped_date <= required_date then 'On Time'
+        else 'Late'
+    end as shipped
+from employees e
+join orders o on o.employee_id = e.employee_id
+group by 
+	e.employee_id, 
+    shipped
+order by 
+	last_name,
+    first_name,
+    num_orders desc
