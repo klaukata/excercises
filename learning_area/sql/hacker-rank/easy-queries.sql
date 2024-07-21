992bf41dcdde19836fd06d8880470beacbf024a3
kboro <kkborowy@gmail.com>
2024-07-21 18:51:09 +0200

    Query a list of CITY and STATE from the STATION table.

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -33,5 +33,13 @@ select name
 from city
 where countrycode = 'JPN'
 
+-- Query a list of CITY and STATE from the STATION table. 
+-- LINK: https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+select city, state
+from station
+
+-- 
+-- LINK: 
+
 -- 
 -- LINK: 
