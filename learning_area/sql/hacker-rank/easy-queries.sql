894d2bb0f8c59f17b16eb018be397acd95cfade5
kboro <kkborowy@gmail.com>
2024-07-21 18:34:00 +0200

    Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -4,3 +4,10 @@
 select * 
 from city
 where countrycode = 'USA' and population > 100000
+
+-- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA. 
+-- LINK: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+select name from city where population > 120000 and countrycode = 'USA';
+
+-- 
+-- LINK: 
