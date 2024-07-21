b39ebf0147074fa9b0a4d4d52c8ee8f10bb6f4cb
kboro <kkborowy@gmail.com>
2024-07-21 18:52:56 +0200

    Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -38,8 +38,11 @@ where countrycode = 'JPN'
 select city, state
 from station
 
--- 
--- LINK: 
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer. 
+-- LINK: https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
+select distinct city
+from station
+where id % 2 = 0
 
 -- 
 -- LINK: 
