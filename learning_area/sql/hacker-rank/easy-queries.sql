ec37e56562ef675eb3a1b991aa67ff314c3295dd
kboro <kkborowy@gmail.com>
2024-07-21 19:18:01 +0200

    Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than per month who have been employees for less than months. Sort your result by ascending employee_id.

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -66,8 +66,13 @@ select name
 from employee
 order by name
 
--- 
--- LINK: 
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than per month 
+-- who have been employees for less than months. Sort your result by ascending employee_id.
+-- LINK: https://www.hackerrank.com/challenges/salary-of-employees/problem?isFullScreen=true
+select name
+from employee
+where salary > 2000 and months < 10
+order by employee_id
 
 -- 
 -- LINK: 
