c0a8d7576fb73de93608db3c619ed74e3c1116dd
kboro <kkborowy@gmail.com>
2024-07-21 18:56:21 +0200

    Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -44,5 +44,19 @@ select distinct city
 from station
 where id % 2 = 0
 
+-- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. 
+-- LINK: https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
+select count(city) - count(distinct city)
+from station
+
+-- 
+-- LINK: 
+
+-- 
+-- LINK: 
+
+-- 
+-- LINK: 
+
 -- 
 -- LINK: 
