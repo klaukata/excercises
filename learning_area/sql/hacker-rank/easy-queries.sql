d1c111856d3487ede8d2d9067082d69fd4db80a1
kboro <kkborowy@gmail.com>
2024-07-21 18:30:46 +0200

    Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA

---
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -0,0 +1,6 @@
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA. 
+-- LINK: https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+
+select * 
+from city
+where countrycode = 'USA' and population > 100000
