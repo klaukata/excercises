72bf51f42119f6794e3c23f0e9184751ecbff8e2
kboro <kkborowy@gmail.com>
2024-07-21 19:13:59 +0200

    Query the Name of any student in STUDENTS who scored higher than Marks

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -49,8 +49,16 @@ where id % 2 = 0
 select count(city) - count(distinct city)
 from station
 
--- 
--- LINK: 
+-- Query the Name of any student in STUDENTS who scored higher than Marks. Order your output by the last three characters of each name. 
+-- If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+-- LINK: https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true
+-- todo
+select name
+from students
+where marks > 75
+order by 
+    right(name, 3),
+    id
 
 -- 
 -- LINK: 
