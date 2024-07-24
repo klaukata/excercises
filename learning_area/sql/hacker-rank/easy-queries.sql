24aca784c6bd7e963ff09bece03ec866a3c1846c
kboro <kkborowy@gmail.com>
2024-07-24 10:41:50 +0200

    Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places.

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -211,8 +211,11 @@ where LAT_N < 137.2345
 order by LAT_N desc
 limit 1
 
--- 
--- LINK:
+-- Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places.
+-- LINK: https://www.hackerrank.com/challenges/weather-observation-station-16/problem?isFullScreen=true
+select round(min(LAT_N), 4)
+from station
+where LAT_N > 38.7780
 
 -- 
 -- LINK:
