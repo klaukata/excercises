7739046a130104fd281ac0873e54d558be48c978
kboro <kkborowy@gmail.com>
2024-07-24 10:23:46 +0200

    Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -187,5 +187,23 @@ where district = 'California'
 select round(avg(population))
 from city
 
+-- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+-- LINK: https://www.hackerrank.com/challenges/japan-population/problem?isFullScreen=true
+select sum(population)
+from city
+where countrycode = 'JPN'
+
+-- 
+-- LINK:
+
+-- 
+-- LINK:
+
+-- 
+-- LINK:
+
+-- 
+-- LINK:
+
 -- 
 -- LINK:
