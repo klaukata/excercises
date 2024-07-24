0607e461a337dc746309776ba4162a485b8e94d3
kboro <kkborowy@gmail.com>
2024-07-24 10:39:09 +0200

    Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places

--- learning_area/sql/hacker-rank/easy-queries.sql
+++ learning_area/sql/hacker-rank/easy-queries.sql
@@ -203,8 +203,13 @@ group by total_earnings
 order by total_earnings desc
 limit 1
 
--- 
--- LINK:
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places.
+-- LINK: https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true
+select round(LONG_W, 4)
+from station
+where LAT_N < 137.2345
+order by LAT_N desc
+limit 1
 
 -- 
 -- LINK:
