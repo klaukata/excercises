a2b3c9eaea694944a60594ee40814d4121f11dbf
kboro <kkborowy@gmail.com>
2024-11-01 14:55:13 +0100

    Print the Elements of a Linked List

--- learning_area/python/hacker-rank/data_structures.py
+++ learning_area/python/hacker-rank/data_structures.py
@@ -6,3 +6,11 @@ def reverseArray(a):
     for i in range(len(a) - 1, -1, -1):
         res.append(a[i])
     return res
+
+# Print the Elements of a Linked List
+# LINK - https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem?isFullScreen=true
+def printLinkedList(head):
+    current = head
+    while current:
+        print(current.data)
+        current = current.next
