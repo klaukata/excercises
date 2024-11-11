f74e95748c8c2a6bb99a8a67ec41233bcbcecf1d
kboro <kkborowy@gmail.com>
2024-11-11 18:39:31 +0100

    Subarray Division 2

--- learning_area/python/hacker-rank/week3.py
+++ learning_area/python/hacker-rank/week3.py
@@ -10,3 +10,16 @@ def twoArrays(k, A, B):
             res = 'NO'
             break
     return res
+
+# Subarray Division 2
+# LINK - https://www.hackerrank.com/challenges/three-month-preparation-kit-the-birthday-bar/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
+# TODO 
+def birthday(s, d, m):
+    res = 0
+    for i in range(len(s) - m + 1):
+        sum = 0
+        for j in range(i, i + m):
+            sum += s[j]
+        if sum == d:
+            res += 1
+    return res
