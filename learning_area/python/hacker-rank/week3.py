35acbd0fe2e1e6f7534a1f2f162e1f3b921cb930
kboro <kkborowy@gmail.com>
2024-11-11 16:39:31 +0100

    Permuting Two Arrays

---
+++ learning_area/python/hacker-rank/week3.py
@@ -0,0 +1,12 @@
+# Permuting Two Arrays
+# LINK - https://www.hackerrank.com/challenges/three-month-preparation-kit-two-arrays/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
+def twoArrays(k, A, B):
+    res = 'YES'
+    A.sort()
+    B.sort(reverse=True)
+    for i in range(len(A)):
+        su = A[i] + B[i]
+        if su < k:
+            res = 'NO'
+            break
+    return res
