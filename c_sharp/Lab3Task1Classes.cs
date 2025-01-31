4a7e4b32a573d3be8e6796a102180aa1d2bcce60
klaukata <kkborowy@gmail.com>
2025-01-31 11:49:09 +0100

    task 1a

---
+++ c_sharp/Lab3Task1Classes.cs
@@ -0,0 +1,42 @@
+namespace Task1A
+{
+    //  TASK 1A RELATED
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string View()
+        {
+            return $"Person: {FirstName} {LastName}, aged {Age} years old.";
+        }
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public string PublicationDate { get; set; }
+        public Person Author { get; set; }
+
+        public Book(string title, string publicationDate, Person author)
+        {
+            Title = title;
+            PublicationDate = publicationDate;
+            Author = author;
+        }
+
+        public string View()
+        {
+            return $"Book: {Title}, published {PublicationDate}, written by {Author.FirstName} {Author.LastName}";
+        }
+    }
+
+    
+}
---
+++ c_sharp/lab3.cs
@@ -0,0 +1,14 @@
+// NOTE: All classes are defined in ./Lab3Task1Classes.cs
+
+/* 1a. Create the following classes:
+ *  -   Person with fields: FirstName, LastName, Age, a constructor initializing all fields, and a method View()
+ *  -   Book with fields: Title, Author (of type Person), PublicationDate, and a method View()
+ *  Create various objects of these classes and execute the View() methods. */
+
+using Task1A;
+
+Person george = new Person("George R. R.", "Martin", 76);
+Console.WriteLine(george.View());       // Person: George R. R. Martin, aged 76 years old.
+
+Book got = new Book("A Game of Thrones", "August 1, 1996", george);
+Console.WriteLine(got.View());          // Book: A Game of Thrones, published August 1, 1996, written by George R. R. Martin
