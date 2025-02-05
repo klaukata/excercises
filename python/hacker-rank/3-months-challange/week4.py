913a29d7d319b30a09c5c9d8711d317c515819b6
klaukata <kkborowy@gmail.com>
2025-02-05 11:52:26 +0100

    Number Line Jumps

--- python/hacker-rank/3-months-challange/week4.py
+++ python/hacker-rank/3-months-challange/week4.py
@@ -20,4 +20,20 @@ def rotateLeft(d, arr):
     right_side = arr[:d]
     return left_side + right_side
 
-print(rotateLeft(2, [1,2,3,4,5]))
+#   Number Line Jumps
+#   LINK - https://www.hackerrank.com/challenges/three-month-preparation-kit-kangaroo/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-four
+
+def kangaroo(x1, v1, x2, v2):
+    isRunning = True
+    k1 = [x1, v1]
+    k2 = [x2, v2]
+    vals = [k1, k2] if x1 < x2 else [k2, k1]    # vals = [[smallerX, smallerV], [biggerX, biggerV]]
+    while (isRunning):
+        if vals[0][0] > vals[1][0] or vals[1][1] >= vals[0][1]: # if smallerX > biggerX or biggerV >= smallerV
+            return "NO"
+        vals[0][0] += vals[0][1]    # smallerX = smallerX + smallerV
+        vals[1][0] += vals[1][1]    # biggerX = biggerX + biggerV
+        if vals[0][0] == vals[1][0]:    # if smallerX == biggerX
+            return "YES"
+
+print(kangaroo(0, 2, 5, 3))
