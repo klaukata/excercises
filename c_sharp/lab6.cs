390e19eb8e6e1ff48f8df773fe81a2b9a98400d9
klaukata <kkborowy@gmail.com>
2025-02-20 16:04:55 +0100

    task 2

--- c_sharp/lab6.cs
+++ c_sharp/lab6.cs
@@ -20,3 +20,32 @@ class Program
         }
     }
 }
+
+// 2.   Write a program that reads the contents of a file with a specified name.
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the file name to read the contents from (fill path, extension included): ");
+        string fileName = Console.ReadLine();
+
+        if (File.Exists(fileName)) 
+        {
+            try
+            {
+                string content = File.ReadAllText(fileName);
+                Console.WriteLine($"Content of a file {fileName}: \n{content}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Provided file does not exists.");
+        }
+        
+    }
+}
